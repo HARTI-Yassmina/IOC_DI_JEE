9b30badef12faa0638581eb8d3c1948b3c2568eb
HARTI Yassmina <yassmina.harti01@gmail.com>
2024-02-19 13:56:40 +0100

    injection des dependances par instanciation statique

--- src/main/java/ext/DaoImplV2.java
+++ src/main/java/ext/DaoImplV2.java
@@ -1,5 +1,13 @@
 package ext;
 
-public class DaoImplV2 {
+import dao.IDao;
 
+public class DaoImplV2 implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("version capteurs");
+        double temperature = 80 ;
+        return temperature;
+    }
 }
